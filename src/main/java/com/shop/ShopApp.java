01e36c90c7e0f0a098dc798b72c64f971e2ba46b
holdrain47 <holdrain47@gmail.com>
2025-11-07 10:49:13 +0900

    CREATE ShopManager

--- src/main/java/com/shop/ShopApp.java
+++ src/main/java/com/shop/ShopApp.java
@@ -1,4 +1,6 @@
 package com.shop;
 
+import com.shop.utils.IdGenerator;
+
 public class ShopApp {
 }
--- src/main/java/com/shop/utils/IdGenerator.java
+++ src/main/java/com/shop/utils/IdGenerator.java
@@ -1,4 +1,36 @@
 package com.shop.utils;
 
 public class IdGenerator {
+  private static IdGenerator instance;  // 싱글톤 인스턴스
+  private int productIdCounter;         // 상품 ID 카운터
+  private int orderIdCounter;            // 주문 ID 카운터
+
+
+  private IdGenerator() {
+    this.productIdCounter = 1;
+    this.orderIdCounter = 1;
+  }
+  //싱글톤 인스턴스 반환
+  //instance가 null이면 새로 생성
+  public static IdGenerator getInstance() {
+    if(instance == null) {
+      instance = new IdGenerator();
+
+    }
+    return instance;
+  }
+
+
+  //"P1", "P2", "P3" 형식으로 생성
+  // return "P" + productIdCounter++;
+  public String generateProductId(){
+    return "P" + productIdCounter++;
+  }
+
+
+  //"O1", "O2", "O3" 형식으로 생성
+  //return "O" + orderIdCounter++;
+  public String generateOrderId(){
+    return "O" + orderIdCounter++;
+  }
 }
